187dab448c39b15c53816fa32df7ccf6998ff8df
mewmew <rnd0x00@gmail.com>
2014-02-06 17:34:04 +0100

    sfml: Add newView function.

--- cgo.go
+++ cgo.go
@@ -21,6 +21,7 @@ func sfmlColor(c color.Color) C.sfColor {
 	return sfmlCol
 }
 
+// sfmlIntRect returns a C sfIntRect based on the provided Go image.Rectangle.
 func sfmlIntRect(r image.Rectangle) C.sfIntRect {
 	rect := C.sfIntRect{
 		left:   C.int(r.Min.X),
@@ -29,4 +30,43 @@ func sfmlIntRect(r image.Rectangle) C.sfIntRect {
 		height: C.int(r.Dy()),
 	}
 	return rect
+}
+
+// sfmlFloatRect returns a C sfFloatRect based on the provided Go
+// image.Rectangle.
+func sfmlFloatRect(r image.Rectangle) C.sfFloatRect {
+	sfmlRect := C.sfFloatRect{
+		left:   C.float(r.Min.X),
+		top:    C.float(r.Min.Y),
+		width:  C.float(r.Dx()),
+		height: C.float(r.Dy()),
+	}
+	return sfmlRect
+}
+
+// sfmlView defines a camera in the 2D scene. A view is composed of a source
+// rectangle, which defines what part of the 2D scene is shown, and a target
+// viewport, which defines where the contents of the source rectangle will be
+// displayed on the render target.
+type sfmlView struct {
+	view *C.sfView
+}
+
+// newView returns a C sfView based on the provided destination rectangle and
+// source point. The scale between the view and the viewport is one to one.
+//
+// Note: The free method of the view must be called when finished using it.
+func newView(dr image.Rectangle, sp image.Point) (view *sfmlView) {
+	view = new(sfmlView)
+	sr := image.Rect(sp.X, sp.Y, sp.X+dr.Dx(), sp.Y+dr.Dy())
+	srcRect := sfmlFloatRect(sr)
+	view.view = C.sfView_createFromRect(srcRect)
+	viewportRect := sfmlFloatRect(dr)
+	C.sfView_setViewport(view.view, viewportRect)
+	return view
+}
+
+// free frees the view.
+func (view *sfmlView) free() {
+	C.sfView_destroy(view.view)
 }

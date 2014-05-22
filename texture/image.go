dd8b3c0866daf4dae8f977add9be3d10e0142e68
mewmew <rnd0x00@gmail.com>
2014-05-22 16:20:38 +0200

    texture: Implement the Read method of Image.

--- texture/image.go
+++ texture/image.go
@@ -8,6 +8,10 @@ import (
 	"errors"
 	"fmt"
 	"image"
+	"image/draw"
+	"log"
+	"time"
+	"unsafe"
 )
 
 // Image represent a read-only texture. It implements the wandi.Image interface.
@@ -39,8 +43,52 @@ func Load(path string) (tex Image, err error) {
 }
 
 // Read reads the provided image and converts it into a read-only texture.
-func Read(img image.Image) (tex Image, err error) {
-	panic("not yet implemented")
+//
+// Note: The Free method of the texture must be called when finished using it.
+func Read(src image.Image) (tex Image, err error) {
+	rgba, ok := src.(*image.RGBA)
+	if !ok {
+		// Use fallback conversion for unknown image formats.
+		return Read(fallback(src))
+	}
+
+	width, height := rgba.Rect.Dx(), rgba.Rect.Dy()
+	if rgba.Stride != 4*width {
+		// Use fallback conversion for subimages.
+		return Read(fallback(src))
+	}
+
+	// Create a new texture based on the pixels of the image.
+	tex.tex = C.sfTexture_create(C.uint(width), C.uint(height))
+	if tex.tex == nil {
+		return Image{}, fmt.Errorf("texture.Read: unable to create %dx%d texture", width, height)
+	}
+	pix := (*C.sfUint8)(unsafe.Pointer(&rgba.Pix[0]))
+	C.sfTexture_updateFromPixels(tex.tex, pix, C.uint(width), C.uint(height), 0, 0)
+
+	// Create a sprite for the texture.
+	tex.sprite = C.sfSprite_create()
+	if tex.sprite == nil {
+		return Image{}, errors.New("texture.Read: unable to create sprite")
+	}
+	C.sfSprite_setTexture(tex.sprite, tex.tex, C.sfTrue)
+
+	return tex, nil
+}
+
+// fallback converts the provided image or subimage into a RGBA image.
+func fallback(src image.Image) *image.RGBA {
+	start := time.Now()
+
+	// Create a new RGBA image and draw the source image onto it.
+	bounds := src.Bounds()
+	dr := image.Rect(0, 0, bounds.Dx(), bounds.Dy())
+	dst := image.NewRGBA(dr)
+	draw.Draw(dst, dr, src, bounds.Min, draw.Src)
+
+	log.Println("texture.fallback: fallback conversion for non-RGBA image (%T) finished in: %v.\n", src, time.Since(start))
+
+	return dst
 }
 
 // Free frees the texture.
